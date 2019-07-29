87e1cfae58dab02535d488639421f2ccc6c08af8
Gang-Ryung Uh <gangryunguh@gmail.com>
2019-07-29 13:39:36 -0500

    Palindrome lab exercise

---
+++ palindrome.cpp
@@ -0,0 +1,23 @@
+//
+//  Palindrome is a word, phrase, or sequence that reads the same backward as forward,
+//  e.g., "madam" or "nurses run".
+//
+//  Implement a program that determines whether a C-string you read from the keyboard
+//  is a palindrome or not.
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    char line[51];
+    int length, count = 0;
+
+    cout << "Enter a string of no more than 50 characters" << endl;
+
+    // add your code below:
+
+
+
+    return 0;
+}
